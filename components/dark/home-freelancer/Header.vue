04c07a5b3eedf1b777227b41898fcf688ac5934e
fernandogonzalezpaolini <fgpaolini@gmail.com>
2025-09-13 21:12:12 +0200

    First Commit

---
+++ components/dark/home-freelancer/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header freelancer" data-scroll-index="0">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <div class="img">
+            <img src="/dark/assets/imgs/intro/freelancer.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="caption">
+      <h1 class="up stroke">
+        <span>FULL STACK</span> <span>DEVELOPER</span>
+      </h1>
+      <h1 class="down"><span>FULL STACK</span> <span>DEVELOPER</span></h1>
+    </div>
+  </header>
+</template>
